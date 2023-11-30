4fe8e5b9524fe2f329fe743279ffb69c96ee36da
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2023-11-30 16:19:14 +0430

    icon added

--- slider/src/App.js
+++ slider/src/App.js
@@ -3,12 +3,16 @@ import "./app.css";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Main />
+      <Footer />
     </>
   );
 };
--- slider/src/Components/Navbar/Navbar.jsx
+++ slider/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./navbar.css";
+import { MdOutlineTravelExplore } from "react-icons/md";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              <MdOutlineTravelExplore className="icon" />
+              Travel.
+            </h1>
+          </a>
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default Navbar;
