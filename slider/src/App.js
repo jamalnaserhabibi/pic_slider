c0f05dfadf0b737108421b2f6ba24484ed985dab
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2023-11-30 07:47:44 +0430

    sync

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./app.css";
+
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+
 const App = () => {
   return (
-    <div Classname="App">
-      <h1>Hello JNH</h1>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 };
 export default App;
